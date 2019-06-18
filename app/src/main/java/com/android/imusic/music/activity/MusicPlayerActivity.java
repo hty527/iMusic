3be3bfd29483dc235f957254482a3faa90c9dc0b
Yuye <584312311@qq.com>
2019-06-18 22:30:28 +0800

    视频播放器界面状态栏处理

--- app/src/main/java/com/android/imusic/music/activity/MusicPlayerActivity.java
+++ app/src/main/java/com/android/imusic/music/activity/MusicPlayerActivity.java
@@ -87,13 +87,8 @@ public class MusicPlayerActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         //这里就是全屏
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
         }
         setContentView(R.layout.music_activity_player);
         initViews();
@@ -342,6 +337,16 @@ public class MusicPlayerActivity extends AppCompatActivity implements
             }
         }
         return smoothScroll;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
     }
 
     @Override
