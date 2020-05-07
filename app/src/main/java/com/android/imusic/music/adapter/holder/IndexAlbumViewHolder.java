14aa97349237085ac6b6d6952748313d780c8059
Yuye <584312311@qq.com>
2020-05-07 18:01:05 +0800

    develop optimize add

--- app/src/main/java/com/android/imusic/music/adapter/holder/IndexAlbumViewHolder.java
+++ app/src/main/java/com/android/imusic/music/adapter/holder/IndexAlbumViewHolder.java
@@ -1,10 +1,13 @@
 package com.android.imusic.music.adapter.holder;
 
+import android.os.Build;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import com.android.imusic.R;
-import com.music.player.lib.view.MusicRoundImageView;
+import com.android.imusic.music.view.LayoutProvider;
+import com.music.player.lib.util.MusicUtils;
 
 /**
  * hty_Yuye@Outlook.com
@@ -13,14 +16,17 @@ import com.music.player.lib.view.MusicRoundImageView;
 
 public class IndexAlbumViewHolder extends RecyclerView.ViewHolder {
 
-    public MusicRoundImageView imageCover;
+    public ImageView imageCover;
     public TextView textTitle;
     public TextView textAnchor;
     public View itemRootView;
 
     public IndexAlbumViewHolder(View itemView) {
         super(itemView);
-        imageCover = (MusicRoundImageView) itemView.findViewById(R.id.view_item_cover);
+        imageCover = (ImageView) itemView.findViewById(R.id.view_item_cover);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            imageCover.setOutlineProvider(new LayoutProvider(MusicUtils.getInstance().dpToPxInt(imageCover.getContext(),5f)));
+        }
         textTitle = (TextView) itemView.findViewById(R.id.view_item_title);
         textAnchor = (TextView) itemView.findViewById(R.id.view_item_anchor);
         itemRootView = itemView.findViewById(R.id.item_root_view);
